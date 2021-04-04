f73d93bea9b4dfa7252c6b9f77770162f7a1cf4a
RenanaLevy <73184239+RenanaLevy@users.noreply.github.com>
2021-04-04 08:10:07 +0300

    Revert "with main2_1, but it doesn't compile"

--- First Task/task_b/main2_2.c
+++ First Task/task_b/main2_2.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <sched.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-#define STACK_SIZE 10000
-
-int grand_child(void* text)
-{
-    printf("grand child process");
-}
-
-int child(void* text)
-{
-    int clone_id = clone(grand_child, STACK_SIZE, CLONE_PARENT, 0);
-    printf("child process");
-}
 
 int main(int argc, int argv[])
 {
-    int clone_id = clone(child, STACK_SIZE, CLONE_PARENT, 0);
+
+
+
+
     return 0;
 }
